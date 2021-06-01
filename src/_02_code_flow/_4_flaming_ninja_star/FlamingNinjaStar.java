b5d3cd8c3de7e0b2c4986aa28c722df8ed0f12a1
student <student@ip-172-31-51-133.us-west-2.compute.internal>
2021-05-31 18:27:44 -0700

    flaming ninja star, greeter, madlibs, and world domination

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -6,47 +6,58 @@ import java.awt.Color;
 public class FlamingNinjaStar {
 	public static void main(String[] args) {
 
-		int baseSize = 200;		//the size of the black part of the star
-		int flameSize = 130;		//the length of the flaming arms
-		
+		int baseSize = 200; // the size of the black part of the star
+		int flameSize = 130; // the length of the flaming arms
+
 		// Make a new robot, and set it's pen down.
-Robot r = new Robot();
+		Robot r = new Robot();
+		r.penDown();
 		// Set the robot speed to 100
 		r.setSpeed(100);
 		// Set the robot window size to 800 x 800
-r.setWindowSize(800, 800);
-		//  COUNT. Make another int variable to count how many times the loop has repeated
-		//         Set its start value to zero.
+		r.setWindowSize(800, 800);
+		// COUNT. Make another int variable to count how many times the loop has
+		// repeated
+		// Set its start value to zero.
 		int count = 0;
-	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-while (count < 1) {
-			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
-		r.turn(45);
-			   // MOVE           Move the robot 64 pixels
-r.move(64);
-			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-		r.turn(-40);
-			   // DRAW FLAME     Move the robot the distance in the variable flameSize
-			
-				//               Turn the robot 170 degrees
+		// LOOP. Start a while loop to repeat all of the code below ONE time (we will
+		// change this later)
+		while (count < 25) {
+			// TURN RIGHT Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full
+			// circle)
+			r.turn(45);
+			// MOVE Move the robot 64 pixels
+			r.move(64);
+			// TURN LEFT Turn the robot 40 degrees to the LEFT. (Negative numbers will turn
+			// the robot counter-clockwise.)
+			r.turn(-40);
+			// DRAW FLAME Move the robot the distance in the variable flameSize
+			r.setPenColor(Color.red);
+			r.move(flameSize);
+			// Turn the robot 170 degrees
 			r.turn(170);
-				//               Move the robot the distance in the variable flameSize (again)
-			
-			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
-				// MOVE         Move the robot the distance in the variable baseSize
-		
-				//  INCREASE COUNT. Increase the count by 1
-		
-		//  End the while loop here
-}
-		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
-		//        This is one arm of the ninja star.
-		
-		// COLOR  Change the Robot's pen color so that the flame is a different color to the rest of the star.
-		//        Run the program again. Check the second picture in the recipe.
+			// Move the robot the distance in the variable flameSize (again)
+			r.move(flameSize);
+			// TURN RIGHT Turn the robot 64 degrees to the right
+			r.turn(64);
+			// MOVE Move the robot the distance in the variable baseSize
+			r.setPenColor(Color.black);
+			r.move(baseSize);
+			// INCREASE COUNT. Increase the count by 1
+			count++;
+			// End the while loop here
+		}
+		r.hide();
+		// TEST Run the program. Check that your shape is the same as the first picture
+		// in the recipe.
+		// This is one arm of the ninja star.
 
-		// LOOP   When you have one arm looking right, change your loop to repeat 25 times and run the program.
-		
+		// COLOR Change the Robot's pen color so that the flame is a different color to
+		// the rest of the star.
+		// Run the program again. Check the second picture in the recipe.
+
+		// LOOP When you have one arm looking right, change your loop to repeat 25 times
+		// and run the program.
+
 	}
 }
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -1,6 +1,5 @@
 package _03_print_and_popups._3_world_domination;
 
-
 import javax.swing.JOptionPane;
 
 public class WorldDomination {
@@ -8,9 +7,14 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+String input = JOptionPane.showInputDialog("Know how to code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if(input.equalsIgnoreCase("yes")) {
+	JOptionPane.showMessageDialog(null, "You will rule the world!");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Sign up for classes at the league!");
+}
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
